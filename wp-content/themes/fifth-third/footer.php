7491e0abbec95cd5442c8724cd0d691d971c1a4d
Dave Ackerman <dmackerman@gmail.com>
2012-10-10 17:38:47 -0400

    footer links

--- wp-content/themes/fifth-third/footer.php
+++ wp-content/themes/fifth-third/footer.php
@@ -1,6 +1,17 @@
 <footer>
 	<div class="inner">
-		<p>Copyright © 2012 Fifth Third Bank, All Rights Reserved<br />
-			Member FDIC</p>
+		<div class="copyright">
+			<p>Copyright © 2012 Fifth Third Bank, All Rights Reserved<br />
+				Member FDIC</p>
+		</div>
+		<div class="links">
+			<ul>
+				<li><a href="#">Alternate Entry</a></li>
+				<li>|</li>
+				<li><a href="#">Official Rules</a></li>
+				<li>|</li>
+				<li><a href="#">Privacy Policy</a></li>
+			</ul>
+		</div>
 	</div>
 </footer>
