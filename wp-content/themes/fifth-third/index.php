bf336c8af194fde0b224e2c1053d4937aab883d2
Dave Ackerman <dmackerman@gmail.com>
2012-10-10 18:02:34 -0400

    added modal script

--- wp-content/themes/fifth-third/index.php
+++ wp-content/themes/fifth-third/index.php
@@ -42,8 +42,8 @@
 
 <!-- JS
 ================================================== -->
-<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-<script src="<?php echo get_bloginfo('template_directory') ?>/js/tabs.js"></script>
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<script src="<?php echo get_bloginfo('template_directory') ?>/js/jquery.simplemodal.1.4.3.min.js"></script>
 
 <!-- Favicons
 ================================================== -->
@@ -59,12 +59,13 @@
     <section role="main" class="main">
       <div class="inner">
         <div class="left">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>Between Nov. 1 – Dec. 30, 2012</h2>
+          <div class="item">
+            <p>Every time you use your Fifth Third Bank Credit or Debit Card this holiday season you’re automatically entered to win!</p>
+          </div>
+          <div class="item">
+            <p>We’re selecting <strong>100 customers</strong> to win a “Holiday Payback” – a statement credit for every card purchase made that day -  <strong>up to $2,000</strong></p>
+          </div>
         </div>
         <div class="right">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
