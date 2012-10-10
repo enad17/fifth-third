6eeb716adaca418bae45c2f184727cf730e8c4cc
Dave Ackerman <dmackerman@gmail.com>
2012-10-10 17:22:23 -0400

    footer initial

--- wp-content/themes/fifth-third/index.php
+++ wp-content/themes/fifth-third/index.php
@@ -54,13 +54,20 @@
 
 </head>
 <body>
-  <div class="container">
-      <div class="inner">
-      
-    
+  <div class="wrap">
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
       
       <footer class="sixteen columns">
-          <p>footer</p>
+          <div class="inner">
+            <p>Copyright.</p>
+          </div>
       </footer>
 
     </div> <!-- end .inner. -->
