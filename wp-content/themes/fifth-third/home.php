8284dd118ce42b6d969eed7d35d447b8c32b8b11
Dave Ackerman <dmackerman@gmail.com>
2012-10-12 12:38:56 -0400

    start blocks

--- wp-content/themes/fifth-third/home.php
+++ wp-content/themes/fifth-third/home.php
@@ -18,12 +18,15 @@
           <a class="callToAction" href="#">See Details</a>
         </div>
         <div class="right">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div class="block">
+            <div class="sprite-green-border"></div>
+          </div>
+          <div class="block">
+            <div class="sprite-green-border"></div>
+          </div>
+          <div class="block no-margin">
+            <div class="sprite-green-border"></div>
+          </div>
         </div>
       </div>
     </section>
