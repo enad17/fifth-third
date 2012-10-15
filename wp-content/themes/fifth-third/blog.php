e4db7c20f76cbeb5d3ce202bf337ca99dba12678
Dave Ackerman <dmackerman@gmail.com>
2012-10-15 15:30:52 -0400

    article layout

--- wp-content/themes/fifth-third/blog.php
+++ wp-content/themes/fifth-third/blog.php
@@ -44,7 +44,7 @@ Template Name: Blog
                 <div class="text">
                   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                   <div class="excerpt">
-                    <?php the_excerpt(); ?>
+                    <?php echo shorten_excerpt(get_the_excerpt(get_the_ID()), 80); ?>
                   </div>
                   <div class="meta">
                     <a class="readMoreLink" href="<?php the_permalink() ?>">Read More</a>
--- wp-content/themes/fifth-third/functions.php
+++ wp-content/themes/fifth-third/functions.php
@@ -6,5 +6,16 @@ add_theme_support( 'post-thumbnails' );
 // Add default posts and comments RSS feed links to head
 add_theme_support( 'automatic-feed-links' );
 
+function shorten_excerpt($text, $num_chars)
+{
+    if (strlen($text) > $num_chars) {
+        $subtext = substr($text, 0, $num_chars);
+        $space = strrpos($subtext, " ");
+        $finaltext = substr($subtext, 0, $space)." [...]";
+        return $finaltext;
+    } else {
+        return $text;    
+    }
+}
 
 ?>
